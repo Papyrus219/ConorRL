1e7257a30e315886b40681503dd1c47f8612848c
Papyrus219 <Papyrus219@o2.pl>
2025-11-03 08:16:47 +0100

    Fixed audio

--- src/system/audio/audio_manager.cpp
+++ src/system/audio/audio_manager.cpp
@@ -6,16 +6,22 @@ using namespace conor;
 
 conor::Audio_manager::Audio_manager(std::string audio_path, int enemies_amount)
 {
+
+    atack_sound_effects_buffers.reserve(enemies_amount+1);
+    atack_sound_effects.reserve(enemies_amount+1);
     for(int i=0;i<enemies_amount+1;i++)
     {
         atack_sound_effects_buffers.emplace_back();
         if(!atack_sound_effects_buffers[i].loadFromFile(audio_path + "sound effects/atack/atack" + std::to_string(i+1) + ".wav") )
             std::cerr << "Kurwa mać...\n";
         atack_sound_effects.emplace_back( atack_sound_effects_buffers[i] );
+        atack_sound_effects[i].setBuffer( atack_sound_effects_buffers[i] );
 
-        atack_sound_effects[i].setVolume(200);
+        atack_sound_effects[i].setVolume(100);
     }
 
+    dead_sound_effects_buffers.reserve(enemies_amount);
+    dead_sound_effects.reserve(enemies_amount);
     for(int i=0;i<enemies_amount;i++)
     {
         dead_sound_effects_buffers.emplace_back();
@@ -23,11 +29,13 @@ conor::Audio_manager::Audio_manager(std::string audio_path, int enemies_amount)
             std::cerr << "Kurwa mać...\n";
 
         dead_sound_effects.emplace_back(dead_sound_effects_buffers[i]);
+        dead_sound_effects[i].setBuffer( dead_sound_effects_buffers[i] );
 
-        dead_sound_effects[i].setVolume(200);
+        dead_sound_effects[i].setVolume(100);
     }
 
     dungeon_music.openFromFile(audio_path + "music/dungeon.wav");
+    dungeon_music.setVolume(70);
 }
 
 void conor::Audio_manager::onNotify(Event event, std::shared_ptr<Being>& entity)
@@ -40,7 +48,6 @@ void conor::Audio_manager::onNotify(Event event, std::shared_ptr<Being>& entity)
             break;
         case Event::Enemy_attack:
         {
-            std::cerr << "Enemy atack!, species = " << (int)entity->species << "\n";
             switch(entity->species)
             {
                 case Enemy::Species::goblin:
